fb1ba2a390fb99fd9de72fe6e2c547ba12282ee3
MichaelRemnev <98388652+MichaelRemnev@users.noreply.github.com>
2022-12-18 20:33:56 +0700

    Add files via upload

---
+++ lab1/lab1_gpu.c
@@ -0,0 +1,40 @@
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+//#include <cmath>
+
+#define FLOAT_TYPE float
+
+FLOAT_TYPE
+compute_sin_on_cpu() 
+{
+    const unsigned int N = pow(10, 6);
+    FLOAT_TYPE sin_array[N];
+    FLOAT_TYPE sin_sum = (FLOAT_TYPE)0.0;
+
+    #pragma acc data create(sin_array) copy(sin_sum)
+    {
+    #pragma acc kernels 
+        for(int i = 0; i < N; i++)
+        {
+            sin_array[i] = sin(2*M_PI*i/N);
+        }
+
+    #pragma acc kernels loop reduction(+:sin_sum)
+        for(int i = 0; i < N; i++) 
+        {
+            sin_sum += sin_array[i];
+        }
+
+    }
+
+    return sin_sum;
+}
+
+int main() 
+{
+    FLOAT_TYPE sin_sum = compute_sin_on_cpu();
+
+    printf("Sum sin = %e\n", sin_sum);
+}
